1d959278016fd2828c957d32bf12c57f91055de0
akash shelar <akash.techinvento@gmail.com>
2021-12-16 11:31:49 +0530

    initial commit

---
+++ Automation.js
@@ -0,0 +1,150 @@
+const puppeteer = require("puppeteer");
+
+const gmailProcess = async function (link) {
+
+  const args = ["--no-sandbox", "--disable-setuid-sandbox"];
+  const browser = await puppeteer.launch({
+    executablePath:
+      "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe",
+    args,
+    devtools: false,
+    javaScriptEnabled: false,
+    permissions: ["notifications"],
+    locale: "en-US",
+    // headless: false,
+    ignoreDefaultArgs: ["--enable-automation", "--disable-infobars"],
+    ignoreHTTPSErrors: true,
+    timeout: 1000 * 60 * 5,
+  });
+
+  const page = await browser.newPage();
+
+  await page.goto(
+    link,
+    {
+      waitUntil: "domcontentloaded",
+      timeout: 1000 * 40 * 2,
+    }
+  );
+  await sleep(3000);
+  let pages1 = await browser.pages();
+  await pages1[0].close();
+
+  //   await page.click(
+  //     "#rc-tabs-0-panel-1 > section:nth-child(1) > div > div:nth-child(7) > div.ant-col.ant-col-24.ant-col-md-19 > div.sc-jcRCNh.jluERy > b > div > img"
+  //   );
+  await sleep(5000);
+  if ((await page.$("#rc-tabs-0-panel-1 > section:nth-child(1) > div > div:nth-child(7) > div.ant-col.ant-col-24.ant-col-md-19 > div > b > div > img")) !== null) {
+    await sleep(3000);
+    await page.click("#rc-tabs-0-panel-1 > section:nth-child(1) > div > div:nth-child(7) > div.ant-col.ant-col-24.ant-col-md-19 > div > b > div > img");
+  }
+  if ((await page.$("#rc-tabs-0-panel-1 > section:nth-child(1) > div > div:nth-child(7) > div.ant-col.ant-col-24.ant-col-md-19 > div.sc-jcRCNh.jluERy > b > div > img")) !== null) {
+  await page.waitForSelector(
+    "#rc-tabs-0-panel-1 > section:nth-child(1) > div > div:nth-child(7) > div.ant-col.ant-col-24.ant-col-md-19 > div.sc-jcRCNh.jluERy > b > div > img"
+  );
+  await page.click(
+    "#rc-tabs-0-panel-1 > section:nth-child(1) > div > div:nth-child(7) > div.ant-col.ant-col-24.ant-col-md-19 > div.sc-jcRCNh.jluERy > b > div > img"
+  );
+  }
+  if ((await page.$("#rc-tabs-0-panel-1 > section:nth-child(1) > div > div:nth-child(7) > div.ant-col.ant-col-24.ant-col-md-19 > div:nth-child(1) > div.sc-iuGLgT.dYpLmi.dark-mode-box > div > b > div > img")) !== null) {
+  await page.waitForSelector(
+    "#rc-tabs-0-panel-1 > section:nth-child(1) > div > div:nth-child(7) > div.ant-col.ant-col-24.ant-col-md-19 > div:nth-child(1) > div.sc-iuGLgT.dYpLmi.dark-mode-box > div > b > div > img"
+  );
+  await page.click(
+    "#rc-tabs-0-panel-1 > section:nth-child(1) > div > div:nth-child(7) > div.ant-col.ant-col-24.ant-col-md-19 > div:nth-child(1) > div.sc-iuGLgT.dYpLmi.dark-mode-box > div > b > div > img"
+  );
+  }
+ 
+  await page.waitForSelector(
+    "#root > section > main > div > article > div > div.ant-col.ant-col-24.ant-col-md-8 > form > span > div > span > input"
+  );
+  await page.type(
+    "#root > section > main > div > article > div > div.ant-col.ant-col-24.ant-col-md-8 > form > span > div > span > input",
+    ""
+  );
+  await page.keyboard.down("Control");
+  await page.keyboard.press("V");
+  await page.keyboard.up("Control");
+ const a = await page.evaluate(() =>    document.querySelector(".ant-input").getAttribute("value") )
+  //   document.querySelector("#root > section > main > div > article > div > div.ant-col.ant-col-24.ant-col-md-8 > form > span > div > span > input").value
+
+  // await browser.close();
+  console.log(a)
+  if (a == null) {
+    return res.status(400).send({status: false, message:"Can not send ID"});
+  }
+  return a;
+};
+
+
+
+const gmailProcess1 = async function (link) {
+
+  const args = ["--no-sandbox", "--disable-setuid-sandbox"];
+  const browser = await puppeteer.launch({
+    executablePath:
+      "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe",
+    args,
+    devtools: false,
+    javaScriptEnabled: false,
+    permissions: ["notifications"],
+    locale: "en-US",
+    // headless: false,
+    ignoreDefaultArgs: ["--enable-automation", "--disable-infobars"],
+    ignoreHTTPSErrors: true,
+    timeout: 1000 * 60 * 5,
+  });
+
+  const page = await browser.newPage();
+
+  await page.goto(
+    link,
+    {
+      waitUntil: "domcontentloaded",
+      timeout: 1000 * 40 * 2,
+    }
+  );
+  await sleep(3000);
+  let pages1 = await browser.pages();
+  await pages1[0].close();
+
+ 
+  await sleep(5000);
+   
+
+  await page.waitForSelector(
+    "#rc-tabs-0-panel-1 > section:nth-child(1) > div > div:nth-child(5) > div.ant-col.ant-col-24.ant-col-md-19 > div > div > div:nth-child(2) > div > a:nth-child(2) > img"
+  );
+  await page.click(
+    "#rc-tabs-0-panel-1 > section:nth-child(1) > div > div:nth-child(5) > div.ant-col.ant-col-24.ant-col-md-19 > div > div > div:nth-child(2) > div > a:nth-child(2) > img"
+  );
+  
+  await page.waitForSelector(
+    "#root > section > main > div > article > div > div.ant-col.ant-col-24.ant-col-md-8 > form > span > div > span > input"
+  );
+  await page.type(
+    "#root > section > main > div > article > div > div.ant-col.ant-col-24.ant-col-md-8 > form > span > div > span > input",
+    ""
+  );
+  await page.keyboard.down("Control");
+  await page.keyboard.press("V");
+  await page.keyboard.up("Control");
+ const a = await page.evaluate(() =>    document.querySelector(".ant-input").getAttribute("value") )
+console.log(a)
+ 
+ 
+  
+  return a;
+};
+module.exports = {
+  gmailProcess1,
+  gmailProcess,
+};
+
+const sleep = (time) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(true);
+    }, time);
+  });
+};
---
+++ app.js
@@ -0,0 +1,74 @@
+const express = require("express");
+const app = express();
+
+const bodyParser = require("body-parser");
+Automation = require("./Automation");
+
+app.use(express.json({ limit: "125mb" }));
+
+const port = 7000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.post("/init", async (req, res) => {
+  let { link } = req.body;
+  let tokenId = await Automation.gmailProcess(link);
+  res.json({
+    status: true,
+    data: tokenId
+  })
+});
+app.post("/init1", async (req, res) => {
+  let { link } = req.body;
+  let tokenId = await Automation.gmailProcess1(link);
+  res.json({
+    status: true,
+    data: tokenId
+  })
+});
+
+
+app.listen(port, async () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+
+process.on("unhandledRejection", (error) => {
+  console.log("unhandledRejection", error.message);
+});
+
+process.on("uncaughtException", (error) => {
+  console.log("uncaughtException", error.message);
+});
+
+process.on("exit", (code) => {
+  console.log(`About to exit with code: ${code}`);
+});
+
+process.on("SIGKILL", function (code) {
+  console.log("SIGKILL received...", code);
+});
+
+process.once("SIGINT", function (code) {
+  console.log("SIGINT received...", code);
+});
+
+process.once("SIGTERM", function (code) {
+  console.log("SIGTERM received...", code);
+});
+
+const checkCpuAndDelay = async () => {
+  var cpu = osu.cpu;
+  const getUsage = await cpu.usage();
+  if (getUsage > 70) {
+    console.log("---CPU USAGE--- ", getUsage);
+    await delay(1000 * 30);
+    return checkCpuAndDelay();
+  }
+  return true;
+};
+
+const delay = (ms) => new Promise((res) => setTimeout(res, ms));
+
+module.exports = app;
